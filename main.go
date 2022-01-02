62038527613a2acacb0982c1fc7330c18d2508a8
worldofelisa <worldofelisa@gmail.com>
2022-01-02 15:56:50 +0000

    adding more books with some similar and some different details

--- main.go
+++ main.go
@@ -16,4 +16,11 @@ func main() {
 	pageNumber = 14
 	grade = 6.5
 	fmt.Println(title, "written by", writer, "drawn by", artist, "published by", publisher, "published in", year, "pages", pageNumber, "condition grade", grade)
+	title = "Epic Vol. 1"
+	writer = "Ryan N. Shawn"
+	artist = "Pheobe Paperclips"
+	year = 2013
+	pageNumber = 160
+	grade = 9.0
+	fmt.Println(title, "written by", writer, "drawn by", artist, "published by", publisher, "published in", year, "pages", pageNumber, "condition grade", grade)
 }
