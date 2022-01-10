a79485c72f67896d7e0c312c4a36a954ad657c60
worldofelisa <worldofelisa@gmail.com>
2022-01-10 17:05:04 +0000

    Refactored the code by adding structs and for loops.

--- main.go
+++ main.go
@@ -4,23 +4,48 @@ import (
 	"fmt"
 )
 
+type Book struct {
+	Title     string
+	Author    string
+	Artist    string
+	Publisher string
+	Year      int
+	Pages     int
+	Condition float64
+}
+
+var bookList = []Book{
+	{
+		Title:     "Mr.GoToSleep",
+		Author:    "Tracey Hatchet",
+		Artist:    "Jewel Tampson",
+		Publisher: "DizzyBooks Publishing Inc.",
+		Year:      1997,
+		Pages:     14,
+		Condition: 6.5,
+	},
+	{
+		Title:     "Epic Vol. 1",
+		Author:    "Ryan N. Shawn",
+		Artist:    "Pheobe Paperclips",
+		Publisher: "DizzyBooks Publishing Inc.",
+		Year:      2013,
+		Pages:     160,
+		Condition: 9.0,
+	},
+	{
+		Title:     "The Adventures of Mr. Man",
+		Author:    "Lord Shellington",
+		Artist:    "Marco Doli",
+		Publisher: "Printing Book Press",
+		Year:      2016,
+		Pages:     55,
+		Condition: 8.7,
+	},
+}
+
 func main() {
-	var publisher, writer, artist, title string
-	var year, pageNumber uint
-	var grade float32
-	title = "Mr.GoToSleep"
-	writer = "Tracey Hatchet"
-	artist = "Jewel Tampson"
-	publisher = "DizzyBooks Publishing Inc."
-	year = 1997
-	pageNumber = 14
-	grade = 6.5
-	fmt.Println(title, "written by", writer, "drawn by", artist, "published by", publisher, "published in", year, "pages", pageNumber, "condition grade", grade)
-	title = "Epic Vol. 1"
-	writer = "Ryan N. Shawn"
-	artist = "Pheobe Paperclips"
-	year = 2013
-	pageNumber = 160
-	grade = 9.0
-	fmt.Println(title, "written by", writer, "drawn by", artist, "published by", publisher, "published in", year, "pages", pageNumber, "condition grade", grade)
+	for _, book := range bookList {
+		fmt.Printf("Title %s\n Written by %s\n Drawn by %s\n Published by %s\n Published in %d\n With %d Pages \n In %.2f Condition \n \n", book.Title, book.Author, book.Artist, book.Publisher, book.Year, book.Pages, book.Condition)
+	}
 }
